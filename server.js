26e3cb9d3ad47b13e775817b7ae5cc8fe3810db9
Akash Pawar <131381080+akashpawar6363@users.noreply.github.com>
2023-05-09 19:36:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/login.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
  });
 
 
